c965c8db89054db1003ab59f9b593ac24a210441
Fernando Kuster <fernandoarielkuster@gmail.com>
2023-11-30 17:34:30 -0300

    Add ejs routes

--- src/controllers/shopControllers.js
+++ src/controllers/shopControllers.js
@@ -1,5 +1,5 @@
 const shopControllers= {
-    shop: (req,res) => res.send('Route for Shop View'),
+    shop: (req, res)=> res.render('shop'),
     item: (req, res)=> res.send(`Route for find a retrieve a producto from an id:${req.params.id}`),
     addItem:  (req, res)=> res.send('Route for add the current item to the shop cart'),
     cart: (req, res)=>res.send('Route for cart view'),
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const shopControllers = require('../controllers/shopControllers')
 
-router.get('/', shopControllers.cart);
+router.get('/', shopControllers.shop);
 router.get('/item/:id', shopControllers.item);
 router.post('/item/:id/add',shopControllers.addItem);
 router.post('/cart', shopControllers.cart);
 router.post('/cart', shopControllers.CheckoutCart);
-
 
 module.exports = router;
