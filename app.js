65938cd6a6b37f8c516eb9ce2128f940bb1bba62
Fernando Kuster <fernandoarielkuster@gmail.com>
2023-12-04 19:47:30 -0300

    Add line comments

--- app.js
+++ app.js
@@ -1,60 +1,70 @@
-// DB TEST
-
-// Database connection configuration
+// Prueba de DB
+// Configuración de la conexión a la base de datos
 const dbConfig = {
-  host: 'sql10.freesqldatabase.com',
-  user: 'sql10627826',
-  password: 'YH6I5nV7ny',
-  database: 'sql10627826',
-  port: 3306,
+  host: 'sql10.freesqldatabase.com', // Dirección del host de la base de datos
+  user: 'sql10627826', // Nombre de usuario para la base de datos
+  password: 'YH6I5nV7ny', // Contraseña para la base de datos
+  database: 'sql10627826', // Nombre de la base de datos
+  port: 3306, // Puerto para la base de datos
 };
 
+// Requerir el módulo mysql
 const mysql = require('mysql');
+// Requerir el módulo express
 const express = require('express');
+// Requerir el módulo path
 const path = require('path');
+// Crear una nueva aplicación express
 const app = express();
+// Definir el puerto en el que se ejecutará el servidor
 const PORT = process.env.PORT || 3000;
 
+// Establecer el motor de vistas a ejs
 app.set('view engine', 'ejs');
+// Establecer la ruta de las vistas
 app.set('views', path.join(__dirname, 'src/views'));
 
-// Create a connection pool
+// Crear un pool de conexiones
 const pool = mysql.createPool(dbConfig);
 
-// Define a route to fetch table names from the database
+// Definir una ruta para obtener los nombres de las tablas de la base de datos
 app.get('/getData', (req, res) => {
-  // Query the database
+  // Consultar la base de datos
   pool.query("SELECT table_name FROM information_schema.tables WHERE table_type='BASE TABLE'", (error, results) => {
     if (error) {
+      // Imprimir el error en la consola
       console.error(error);
+      // Enviar una respuesta de error
       return res.status(500).json({ error: 'Error retrieving data from the database' });
     }
 
-    // Send the results as JSON
+    // Enviar los resultados como JSON
     res.json(results);
   });
 });
 
-// Define a route to fetch products from the database
+// Definir una ruta para obtener productos de la base de datos
 app.get('/getProducts', (req, res) => {
-  // Query the database
+  // Consultar la base de datos
   pool.query("SELECT * FROM product", (error, results) => {
     if (error) {
+      // Imprimir el error en la consola
       console.error(error);
+      // Enviar una respuesta de error
       return res.status(500).json({ error: 'Error retrieving data from the database' });
     }
 
-    // Send the results as JSON
+    // Enviar los resultados como JSON
     res.json(results);
   });
 });
 
-// Serve HTML page
+// Servir la página HTML
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Start the server
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
