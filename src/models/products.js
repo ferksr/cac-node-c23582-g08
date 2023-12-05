759f0d12c91b66b1f6d6dc0de1fd11e08646494a
Johan Pizarro <javier0218@gmail.com>
2023-12-05 19:50:25 -0300

    adding .env

--- src/models/products.js
+++ src/models/products.js
@@ -11,7 +11,23 @@ const getProducts = async () => {
     }
 }
 
+const getProductsByName = async(params) => {
+
+    const {category,product_name, price} = params;
+    try {
+        const [item] = await conn.query('SELECT * FROM product pr join category ca on pr.category_id= ca.category_id where pr.product_name=? and pr.price=? ;',
+        {category, product_name, price});
+        return item;
+    } catch (error){
+        throw error;
+    } finally {
+        conn.releaseConnection();
+    }
+}
+
+
 
 module.exports = {
-    getProducts
+    getProducts,
+    getProductsByName
 }
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const shopControllers = require('../controllers/shopControllers')
 
 router.get('/', shopControllers.shop);
-router.get('/item', shopControllers.item);
+router.get('/item/:id', shopControllers.item);
 router.get('/cart', shopControllers.cart);
 
 module.exports = router;
