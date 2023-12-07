0bcd2cff8e0fde79afe40c0dedd253f7247ea269
Johan Pizarro <javier0218@gmail.com>
2023-12-07 18:58:45 -0300

    adding validation login

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const adminControllers = require('../controllers/adminControllers')
 
-router.get('/admin', adminControllers.admin);
+router.get('/admin/:id', adminControllers.admin);
 router.get('/create', adminControllers.create);
 router.get('/edit/:id', adminControllers.edit);
-router.get('/login', adminControllers.login);
 router.get('/register', adminControllers.register);
 
 module.exports = router;
