021ba514ccd6761a062b2d1e7124a65f020892e5
Johan Pizarro <javier0218@gmail.com>
2023-12-08 16:07:04 -0300

    adding admin models

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,7 +1,15 @@
-
+const products = require('../models/products.js');
 
 const adminControllers = {
-  admin:  (req, res) => res.render('admin') ,
+  admin:  async (req, res) => {     
+    const product= req.params.id;
+     const item_detail = await products.getProductsById(product); 
+     const items = await products.getProducts();  
+     console.log(item_detail)   
+     res.render('admin', {
+         item : item_detail,
+         list: items
+   } )} ,
   create: (req, res) => res.render('create'),
   edit: (req, res) => res.render('edit'),
   register: (req, res) => res.render('register'),
