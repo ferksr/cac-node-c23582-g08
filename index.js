7ed2111d76db3f26cb908344e20151279ba1c608
Fernando Kuster <fernandoarielkuster@gmail.com>
2023-12-09 12:05:26 -0300

    Fix routes (relative to absolute)

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './src/views'));
+app.set('views', path.join(__dirname, '/src/views'));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views');
