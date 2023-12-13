420c0d5a913c9fca8a28a2b236a3b58924b09cf2
Ferksr <fernandoarielkuster@gmail.com>
2023-12-13 10:28:14 -0300

    Show product in edit view

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,7 +5,8 @@ const adminControllers = require('../controllers/adminControllers')
 router.get('/admin/:id', adminControllers.admin);
 router.get('/create', adminControllers.create);
 router.post('/create', adminControllers.createProduct);
-router.get('/edit/:id', adminControllers.editProduct);
+router.get('/edit/:id', adminControllers.edit);
+router.put('/edit/:id', adminControllers.editProduct);
 router.get('/register', adminControllers.register);
 router.delete('/delete/:id', adminControllers.deleteProduct);
 
